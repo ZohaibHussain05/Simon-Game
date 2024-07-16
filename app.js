c5d600805f73857df0e67442c547021721969fe9
Zohaib Hussain <chzohaibjutt300@gmail.com>
2024-07-17 00:29:44 +0500

    [ADD] button flash

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let gamSeq = [];
 let userSeq = [];
+let btns = ["red","yellow","green","blue"]
 
 let gameStart = false;
 let level = 0;
@@ -14,8 +15,19 @@ document.addEventListener("keypress",function(){
     }
 })
 
+function btnFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    },250)
+}
+
 function levelUP(){
     level++;
     h1.innerText = `Level ${level}`;
-    
+
+    //Generate random color from btns array
+    let randomColor = btns[Math.floor(Math.random()*3)];
+    let randomBtn = document.querySelector(`.${randomColor}`);
+    btnFlash(randomBtn);
 }

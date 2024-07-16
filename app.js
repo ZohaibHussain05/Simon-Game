ecfebfef9c1e1d77c45eaa2c5e3f7425a06bcf76
Zohaib Hussain <chzohaibjutt300@gmail.com>
2024-07-16 23:56:25 +0500

    [app.js] Start game eventListner

--- app.js
+++ app.js
@@ -3,4 +3,19 @@ let userSeq = [];
 
 let gameStart = false;
 let level = 0;
+let h1 = document.querySelector("h1");
 
+document.addEventListener("keypress",function(){
+    if(gameStart == false){
+        console.log("Game Started");
+        gameStart = true;
+
+        levelUP();
+    }
+})
+
+function levelUP(){
+    level++;
+    h1.innerText = `Level ${level}`;
+    
+}
